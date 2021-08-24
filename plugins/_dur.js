8a2f43c54b6c9c470ca771a5df9f3dba8b1ce66d
HyroKidsSmp <79112870+HyroKidsSmp@users.noreply.github.com>
2021-08-24 21:58:03 +0700

    Durability.js
    File buat cek durability habis atau gk

---
+++ plugins/_dur.js
@@ -0,0 +1,12 @@
+let handler = m => m
+
+handler.before = function (m) {
+	let inv = global.db.data.invmenu[m.sender]
+	if (inv.durability === 0) {
+	inv.hancur = true
+	return true}
+	else if (inv.durability > 0) {
+		inv.hancur = false
+		return true}
+	}
+module.exports = handler
