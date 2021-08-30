d9e8f60ac281be834fe24a1f35a92a0896a12b68
HyroKidsSmp <79112870+HyroKidsSmp@users.noreply.github.com>
2021-08-30 15:08:15 +0700

    Kayu

--- plugins/_dur.js
+++ plugins/_dur.js
@@ -4,6 +4,7 @@ handler.before = function (m) {
 	let inv = global.db.data.invmenu[m.sender]
 	if (inv.durability === 0) {
 	inv.hancur = true
+        inv.rod = ''
 	return true}
 	else if (inv.durability > 0) {
 		inv.hancur = false
