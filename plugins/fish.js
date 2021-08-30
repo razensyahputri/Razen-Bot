a43e27309581e5dc6339aa0eedfe8532cbbae67b
HyroKidsSmp <79112870+HyroKidsSmp@users.noreply.github.com>
2021-08-31 00:53:45 +0700

    Kata Otak gw:
    MEMPERSULIT DIRI DENGAN GAYA

--- plugins/fish.js
+++ plugins/fish.js
@@ -12,39 +12,54 @@ let handler = async (m, { conn, text, usedPrefix}) => {
     if (mancing.hancur === true) throw 'Pancingan Kamu Rusak, beli yang baru, dengan cara ${usedPrefix}fshop'
     if (mancing.hancur === false) 
     {
+        let haha
+        if (mancing.rod == 'kayu') {
+            haha = 1
+        } else if (mancing.rod == 'batu') {
+            haha = 2
+        }
 	let hoki1 = pickRandom(['1', '2', '3']) 
-	let na = 1 * hoki1
+	let na = 1 * hoki1 * haha
 	let su = pickRandom(['100', '150', '50', '200', '250', '300']) 
 	let anj = su * hoki1
 	let sa = 26
+        let dur
+        let pick = pickRandom(['0', '0.1', '0.2', '0.25', '0.3', '0.4', '0.5', '0.6', '0.7', '0.75', '0.8', '0.9', '1']) 
+        if (mancing.rod == 'kayu') {
+            dur = 1 * 1 * pick * 1 * hoki1
+        } else if (mancing.rod == 'batu') {
+            dur = 1 * 1 * pick * 1 * hoki1
+        } else if (mancing.rod == 'god') {
+            dur = 0 * 0 * pick * 0 * hoki1
+        }
       if (user.hoki < sa) {
       let hasil = pickRandom(['cod', 'tropical', 'salmon', 'Golden', 'cod', 'cod', 'tropical', 'tropical', 'salmon', 'salmon', 'salmon', 'salmon', 'sampah', 'sampah', 'sampah', 'sampah', 'sampah'])
       if (hasil === "sampah") {
       mancing.sampah += na
-      mancing.durability -= 1
+      mancing.durability -= dur
       mancing.xpfish += anj 
       }
       else if (hasil === "cod") {
       mancing.cod += na
-      mancing.durability -= 1
+      mancing.durability -= dur
       mancing.xpfish += anj
       }
       else if (hasil === "tropical") {
       mancing.tropical += na
-      mancing.durability -= 1
+      mancing.durability -= dur
       mancing.xpfish += anj
       }
       else if (hasil === "salmon") {
       	mancing.salmon += na
-          mancing.durability -= 1
+          mancing.durability -= dur
           mancing.xpfish += anj
       } else if (hasil === "Golden") {
       mancing.golden += na
-      mancing.durability -= 1
+      mancing.durability -= dur
       mancing.xpfish += anj
       }
       conn.reply(m.chat, `Berhasil Memancing
-Kamu mendapatkan ðŸŽ£ ${na} ${hasil} dan mendapatkan + ${anj} xpfish`) 
+Kamu mendapatkan ðŸŽ£ ${na} ${hasil} dan mendapatkan + ${anj} xpfish dan durability fishing rod mu -${dur}`) 
       }
       } 
       }
