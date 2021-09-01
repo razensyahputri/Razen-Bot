e75f3ae19a7044d9b0f7338f37e28b4c821fb2bb
HyroKidsSmp <79112870+HyroKidsSmp@users.noreply.github.com>
2021-09-01 11:42:31 +0700

    Tambah Rod??

--- plugins/shopbot.js
+++ plugins/shopbot.js
@@ -3,8 +3,11 @@ function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
 }
 let kays = 5000
-let bats = 20000
-let bass = 50000
+let bats = 2000000
+let bass = 5000000
+let gols = 3000000
+let dias = 10000000
+let nets = 1000000000
 let handler = async (m, { conn, text, usedPrefix}) => {
 	let user = global.db.data.users[m.sender]
         let invu = global.db.data.invmenu[m.sender]
@@ -12,11 +15,25 @@ let handler = async (m, { conn, text, usedPrefix}) => {
 - rod
   - kayu
     - 5000
+    - durability: 100
   - batu
-    - 20000
+    - 2000000
+    - durability: 150
   - besi
-    - 50000
-  - coming soon
+    - 5000000
+    - durability: 200
+  - Gold
+    - 3000000
+    - durability: 175
+  - Diamond
+    - 10000000
+    - durability: 300
+  - Netherite
+    - 1000000000
+    - durability: 500
+  - God
+    - PC OWNER
+    - durability: ?? 
 - bait
 - boat
 - hoki
@@ -51,7 +68,36 @@ let handler = async (m, { conn, text, usedPrefix}) => {
                invu.durabilty += 200
                }
             }
-      }
+        if (text == 'rod gold') {
+           if (invu.moneyfish < gols) throw 'Uang lu gk ada ego'
+           if (invu.moneyfish > gols) {
+               conn.reply(m.chat, `Berhasil membeli pancingan gold`) 
+               invu.rod = 'gold'
+               invu.moneyfish -= gols
+               invu.durabilty += 175
+               }
+            }
+        if (text == 'rod diamond') {
+	
+           if (invu.moneyfish < dias)  throw 'Uang lu gk ada ego'
+           if (invu.moneyfish > dias) {
+               conn.reply(m.chat, `Berhasil membeli pancingan diamond`) 
+               invu.rod = 'diamond'
+               invu.moneyfish -= dias
+               invu.durabilty += 300
+               }
+            }
+        if (text == 'rod netherite'){
+	
+           if (invu.moneyfish < nets) throw 'Uang lu gk ada ego'
+           if (invu.moneyfish > nets) {
+               conn.reply(m.chat, `Berhasil membeli pancingan Netherite`) 
+               invu.rod = '*Netherite*'
+               invu.moneyfish -= nets
+               invu.durabilty += 500
+               }
+            }
+       
   }
 
 handler.help = ['shop']
